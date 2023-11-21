ff0bc39592d471688ab410891d24acc486e1f695
saidazimov720 <saidazimovnurbek01gmail.com>
2023-11-21 20:22:12 +0500

    calculate

--- calculator.py
+++ calculator.py
@@ -5,3 +5,11 @@ def on_button_click(value):
     entry.delete(0, tk.END)
     entry.insert(tk.END, current + value)
     
+    
+def calculate():
+    try:
+        result = eval(entry.get())
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, str(result))
+    except Exception as e:
+        entry.delete
